3f267a52ca1434a6bc6858ca71f24d8652f15b6a
charlesonunze <charlesonunze2@gmail.com>
2020-10-09 00:58:15 +0100

    add cors middleware

--- services/PostService/index.js
+++ services/PostService/index.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 
 const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
 const posts = {};
