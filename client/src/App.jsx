19ef9207eab43b62fac293d1e35fb9cd90f3ef4d
charlesonunze <charlesonunze2@gmail.com>
2020-10-09 09:03:31 +0100

    connect to PostService API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import CreatePost from './CreatePost';
 import PostList from './PostList';
 
 function App() {
+	const [posts, updatePosts] = useState([]);
+
+	const fetctPosts = async () => {
+		const { data } = await axios.get('http://localhost:4000/posts/');
+		updatePosts(Object.values(data));
+	};
+
+	useEffect(() => {
+		fetctPosts();
+	}, []);
+
+	const addPostHandler = async (title) => {
+		const _posts = [...posts];
+
+		const { data: _post } = await axios.post('http://localhost:4000/posts/', {
+			title
+		});
+
+		_posts.unshift(_post);
+		updatePosts(_posts);
+	};
+
 	return (
 		<div className='container' style={{ padding: '30px' }}>
-			<CreatePost />
+			<CreatePost onPostAdd={addPostHandler} />
 			<hr />
 			<PostList />
 		</div>
--- client/src/CreatePost.jsx
+++ client/src/CreatePost.jsx
@@ -1,25 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const CreatePost = () => (
-	<div style={{ width: '300px' }}>
-		<form>
-			<div className='form-group'>
-				<label htmlFor='title'>Post Title</label>
-				<input
-					type='text'
-					id='title'
-					name='title'
-					className='form-control'
-					aria-describedby='title'
-					placeholder='Enter title'
-				/>
-			</div>
+const CreatePost = ({ onPostAdd }) => {
+	const [text, updateText] = useState('');
 
-			<button type='submit' className='btn btn-primary'>
-				Submit
-			</button>
-		</form>
-	</div>
-);
+	return (
+		<div style={{ width: '300px' }}>
+			<form
+				onSubmit={(e) => {
+					e.preventDefault();
+					onPostAdd(text);
+					updateText('');
+				}}
+			>
+				<div className='form-group'>
+					<label htmlFor='title'>Post Title</label>
+					<input
+						type='text'
+						id='title'
+						name='title'
+						value={text}
+						className='form-control'
+						aria-describedby='title'
+						placeholder='Enter post title'
+						onChange={(e) => updateText(e.target.value)}
+					/>
+				</div>
+
+				<button
+					type='submit'
+					className='btn btn-primary'
+					disabled={text.length < 1}
+				>
+					Submit
+				</button>
+			</form>
+		</div>
+	);
+};
 
 export default CreatePost;
