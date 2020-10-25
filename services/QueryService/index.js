79d32b04e5e5306e66e3142f791baf5613700c45
charlesonunze <charlesonunze2@gmail.com>
2020-10-25 20:32:00 +0100

    feat: handle comment updated event

--- services/QueryService/index.js
+++ services/QueryService/index.js
@@ -15,16 +15,27 @@ app.get('/posts', (req, res) => res.send(posts))
 app.post('/events', (req, res) => {
   const { type, data } = req.body;
 
+  console.log('Event Recieved:', type);
+
   if (type === 'POST_CREATED') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] }
   };
 
   if (type === 'COMMENT_CREATED') {
-    const { id, content, postId } = data;
+    const { id, content, postId, status } = data;
     const post = posts[postId];
 
     post.comments.push({ id, content })
+  };
+
+  if (type === 'COMMENT_UPDATED') {
+    const { id, content, postId, status } = data;
+    const post = posts[postId];
+
+    const comment = post.comments.find((c) => c.id === id)
+    comment.status = status
+    comment.content = content
   };
 
   return res.send('OK')
